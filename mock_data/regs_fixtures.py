be06b2b846da57e927cd4e6eca8fd5a4d4d7b3ee
Bloder <aptgetinstalldanielleme@gmail.com>
2018-04-15 15:09:09 -0300

    Change fixture column names

--- mock_data/regs_fixtures.py
+++ mock_data/regs_fixtures.py
@@ -5,10 +5,10 @@ from sklearn import preprocessing
 
 def load_fixture():
     df = pd.read_csv(os.path.dirname(__file__) + '/users_regs.csv', sep=',',
-                names=['user', 'item', 'visits', 'category'], header=None)
-    rating = df['visits'].values.astype(float)
+                     names=['user', 'event', 'score', 'category'], header=None)
+    rating = df['score'].values.astype(float)
     min_max_scaler = preprocessing.MinMaxScaler()
     x_scaled = min_max_scaler.fit_transform(rating.reshape(-1, 1))
     df_normalized = pd.DataFrame(x_scaled)
-    df['visits'] = df_normalized
+    df['score'] = df_normalized
     return df
