d2b43994a4b888a572aeed22c8f9f19847e6af0e
Bloder <aptgetinstalldanielleme@gmail.com>
2018-04-15 15:14:43 -0300

    Load fixture and create autocoders functions

--- rec_model.py
+++ rec_model.py
@@ -1,5 +1,37 @@
 import os
-
 os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 from mock_data import regs_fixtures as rf
+import tensowflow as tf
+
+regs_df = rf.load_fixture()
+
+num_event = regs_df.event.nunique()
+num_user = regs_df.user.nunique()
+num_hidden_1 = 10
+num_hidden_2 = 5
+
+autoencoder_value = tf.placeholder(tf.float64, [None, num_user])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1], dtype=tf.float64)),
+    'encoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2], dtype=tf.float64)),
+    'decoder_h1': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1], dtype=tf.float64)),
+    'decoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_input], dtype=tf.float64)),
+}
+
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
+    'encoder_b2': tf.Variable(tf.random_normal([num_hidden_2], dtype=tf.float64)),
+    'decoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
+    'decoder_b2': tf.Variable(tf.random_normal([num_input], dtype=tf.float64)),
+}
+
+
+def encoder(x):
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
+    return tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
+
+
+def decoder(x):
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
+    return tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
