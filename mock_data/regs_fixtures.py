c63522f6a8a0c8f478405e0505083337fa0e85aa
Bloder <aptgetinstalldanielleme@gmail.com>
2018-04-15 15:06:51 -0300

    Return data frame with converted values

--- mock_data/regs_fixtures.py
+++ mock_data/regs_fixtures.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import os
+from sklearn import preprocessing
+
 
 def load_fixture():
-    pd.read_csv(os.path.dirname(__file__) + '/users_regs.csv')
-
-
-if __name__ == '__main__':
-    load_fixture()
+    df = pd.read_csv(os.path.dirname(__file__) + '/users_regs.csv', sep=',',
+                names=['user', 'item', 'visits', 'category'], header=None)
+    rating = df['visits'].values.astype(float)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(rating.reshape(-1, 1))
+    df_normalized = pd.DataFrame(x_scaled)
+    df['visits'] = df_normalized
+    return df
